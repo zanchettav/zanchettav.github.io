95db01e02418dc4dbb27cc13f65f064ad1e2d49c
zanchettav <159554918+zanchettav@users.noreply.github.com>
2024-10-02 08:07:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'O algoritmo é a receita, ou seja, os comandos em um passo a passo claro e lógico que conduzem as ações para a resolução de um problema ou para atingir determinado objetivo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do paraguai?',
+    'A capital do paraguai é Assunção'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Robótica?',
+    'A robótica é uma ciência da engenharia aplicada que é tida como uma combinação da tecnologia de máquinas operatrizes e ciência da computação. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz maça em Inglês?',
+    'Maça em ingles é Apple'
+)
+
+criaCartao(
+    
+)
