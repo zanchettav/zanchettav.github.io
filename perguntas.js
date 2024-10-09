6aff362818413e1e13acc2986929fcda299d12ed
zanchettav <159554918+zanchettav@users.noreply.github.com>
2024-10-09 11:20:45 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 criaCartao(
     'Programação',
     'O que é um algoritmo?',
-    'O algoritmo é a receita, ou seja, os comandos em um passo a passo claro e lógico que conduzem as ações para a resolução de um problema ou para atingir determinado objetivo.'
+    'O algoritmo é a receita, ou seja, os comandos em um passo a passo claro e lógico que conduzem as ações para a resolução de um problema ou para atingir determinado objetivo.',
 )
 
 criaCartao(
     'Geografia',
     'Qual a capital do paraguai?',
-    'A capital do paraguai é Assunção'
+    'A capital do paraguai é Assunção',
 )
 
 criaCartao(
     'Programação',
     'O que é Robótica?',
-    'A robótica é uma ciência da engenharia aplicada que é tida como uma combinação da tecnologia de máquinas operatrizes e ciência da computação. '
+    'A robótica é uma ciência da engenharia aplicada que é tida como uma combinação da tecnologia de máquinas operatrizes e ciência da computação. ',
 )
 
 criaCartao(
@@ -23,5 +23,37 @@ criaCartao(
 )
 
 criaCartao(
-    
+    'Lingua espanhol',
+    'Como se diz como está em espanhol?',
+    'Como está em espanhol é Cómo estás'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país com mais habitantes?',
+    'O país com mais habitantes é a China'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a √ 121??',
+    'É 11'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Revolução francesa?',
+    'Aconteceu em 5 de maio de 1789'
+)
+
+criaCartao(
+    'História',
+    'Quem  inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Arte',
+    'Quantos anos tem o quadro da monalisa?',
+    'O quadro tem 521 anos'
 )
